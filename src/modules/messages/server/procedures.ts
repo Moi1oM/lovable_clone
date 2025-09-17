f9b9cd262a3e686a0eef7e2d2836ae7eb5dd1593
Moi1oM <hcloud0806@gmail.com>
2025-09-18 00:23:12 +0900

    10: message ui

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -4,23 +4,40 @@ import { baseProcedure, createTRPCRouter } from '@/trpc/init';
 import { z } from 'zod';
 
 export const messagesRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    const messages = await prisma.message.findMany({
-      orderBy: {
-        updatedAt: 'desc',
-      },
-    });
-    return messages;
-  }),
+  getMany: baseProcedure
+    .input(
+      z.object({
+        projectId: z.string().min(1, { message: 'projectId is required' }),
+      }),
+    )
+    .query(async ({ input }) => {
+      const messages = await prisma.message.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+        include: {
+          fragment: true,
+        },
+        orderBy: {
+          updatedAt: 'asc',
+        },
+      });
+      return messages;
+    }),
   create: baseProcedure
     .input(
       z.object({
-        value: z.string().min(1, { message: 'Message is required' }),
+        value: z
+          .string()
+          .min(1, { message: 'Prompt is required' })
+          .max(10000, { message: 'Prompt is too long' }),
+        projectId: z.string().min(1, { message: 'projectId is required' }),
       }),
     )
     .mutation(async ({ input }) => {
       const createdMessage = await prisma.message.create({
         data: {
+          projectId: input.projectId,
           content: input.value,
           role: 'USER',
           type: 'RESULT',
@@ -31,6 +48,7 @@ export const messagesRouter = createTRPCRouter({
         name: 'code-agent/run',
         data: {
           value: input.value,
+          projectId: input.projectId,
         },
       });
 
